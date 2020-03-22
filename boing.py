5ea63dc0e7bd9cb5fe53f29cb1afe992b46660ab
Dan Rolfe <dan@rolfe.email>
2020-03-22 21:23:14 +0000

    Abstract ball and ground into classes

--- boing.py
+++ boing.py
@@ -26,6 +26,113 @@ def ImageToRGB(im, rgb0=[0, 0, 0], rgb=[0, 1, 0]):
         imrgb[:, :, icomp]+=rgb0[icomp]+im*(rgb[icomp]-rgb0[icomp])
     return imrgb/maxfac
 
+class Ground:
+    def __init__(self, point, normal, dx=3, dy=10):
+        self.o=point # Ground passes through this point
+        self.n=normal # Unit normal to ground
+        self.dx=dx
+        self.dy=dy
+
+    def check(self, C, Rx, Ry, Rz):
+        # Lambda for where ray intersects ground
+        lambda_ground=np.sum((self.o-C)*self.n)/(Rx*self.n[0]+Ry*self.n[1]+Rz*self.n[2])
+
+        # If ray intersects ground
+        intersects_ground=(lambda_ground > 0) & np.isfinite(lambda_ground)
+
+        self.intersects=intersects_ground
+        self.lmbda=lambda_ground
+
+    def image(self, C, Rx, Ry, Rz, L, intersects_ground_first):
+        # Point of intersection with ground
+        Gx=C[0]+self.lmbda*Rx
+        Gy=C[1]+self.lmbda*Ry
+        Gz=C[2]+self.lmbda*Rz
+
+        ix=np.floor(Gx/self.dx).astype(int)
+        iy=np.floor(Gy/self.dy).astype(int)
+        ground=intersects_ground_first*np.mod(ix+iy, 2)
+
+        ground_image=ImageToRGB(ground, rgb=[0, 0.3, 0], rgb0=[0, 0, 0])
+        return ground_image
+
+class Ball:
+    def __init__(self, centre, radius, dphi=np.pi/10, dtheta=np.pi/10):
+        self.centre=centre
+        self.radius=radius
+        self.dphi=dphi
+        self.dtheta=dtheta
+
+    def check(self, C, Rx, Ry, Rz):
+        BC=C-self.centre
+
+        RdotBC=Rx*BC[0]+Ry*BC[1]+Rz*BC[2]
+
+        # P = point of closest approach of ray to B
+        BPx=BC[0]-RdotBC*Rx
+        BPy=BC[1]-RdotBC*Ry
+        BPz=BC[2]-RdotBC*Rz
+        magBP=BPx**2+BPy**2+BPz**2
+
+        # If ray intersects ball
+        intersects_ball=magBP <= self.radius**2
+
+        # Lowest of two lambda values for location where rays intersect with surface of ball
+        beta=np.sqrt(RdotBC**2-np.linalg.norm(BC)**2+self.radius**2) # Second term of quadratic eqn soln
+        if not np.array_equal(intersects_ball, np.isfinite(beta)):
+            raise RuntimeError("Expected intersections tests to be identical") # Sanity check
+        lambda_ball=-RdotBC-beta
+
+        self.intersects=intersects_ball
+        self.lmbda=lambda_ball
+
+    def image(self, C, Rx, Ry, Rz, L, intersects_ball_first):
+        # S = first (closest to camera) location where ray intersects surface of ball
+        Sx=C[0]+self.lmbda*Rx
+        Sy=C[1]+self.lmbda*Ry
+        Sz=C[2]+self.lmbda*Rz
+
+        # SL
+        SLx=Sx-L[0]
+        SLy=Sy-L[1]
+        SLz=Sz-L[2]
+
+        # SB
+        SBx=Sx-self.centre[0]
+        SBy=Sy-self.centre[1]
+        SBz=Sz-self.centre[2]
+        phi=np.arctan2(SBy, SBx)
+        theta=np.arctan2(SBz, np.sqrt(SBx**2+SBy**2))
+
+        # Normal to surface at S
+        magSB=np.sqrt(SBx**2+SBy**2+SBz**2)
+        magSL2=SLx**2+SLy**2+SLz**2
+        magSL=np.sqrt(magSL2)
+
+        # alpha = angle between normal to surface (outwards) and line from surface to L
+        cosalpha=-(SBx*SLx+SBy*SLy+SBz*SLz)/(magSB*magSL)
+
+        # Illumination of surface by light 1/R2 law and projection effect of angle
+        lfac=cosalpha/magSL2
+        lfac/=np.nanmax(lfac) # Normalise so max is 1
+
+        # If normal not pointing towards light then no illumination
+        lfac[lfac<0]=0
+
+        # Pattern on ball
+        iphi=np.floor(phi/self.dphi).astype(int)
+        itheta=np.floor(theta/self.dtheta).astype(int)
+        w_noball=np.where(np.logical_not(intersects_ball_first))
+        red_ball=intersects_ball_first*np.mod(iphi+itheta, 2)*lfac
+        red_ball[w_noball]=0
+        white_ball=intersects_ball_first*np.mod(iphi+itheta+1, 2)*lfac
+        white_ball[w_noball]=0
+
+        red_ballimage=ImageToRGB(red_ball, rgb=[1, 0, 0], rgb0=[0, 0, 0])
+        white_ballimage=ImageToRGB(white_ball, rgb=[1, 1, 1], rgb0=[0, 0, 0])
+
+        return red_ballimage+white_ballimage
+
 def Boing():
 
     size_cm=(29.7, 21.0)
@@ -46,13 +153,10 @@ def Boing():
     #
 
     C=np.array([0, 0, 0]) # Camera location
-    B=np.array([0, 150, -4]) # Centre of ball
     L=np.array([5, 130, 5]) # Point source light
 
-    Go=np.array([0, 0, -5]) # Ground pass through this point
-    Gn=np.array([0, 0, 1]) # Unit normal to ground
-
-    ball_radius=5
+    ground=Ground(point=np.array([0, 0, -5]), normal=np.array([0, 0, 1]))
+    ball=Ball(centre=np.array([0, 150, -1.5]), radius=5)
 
     w=2000
     h=2000
@@ -75,96 +179,20 @@ def Boing():
     Rx=Ry*tan_thetaH
     Rz=Ry*tan_thetaV
 
-    # Lambda for where ray intersects ground
-    lambda_ground=np.sum((Go-C)*Gn)/(Rx*Gn[0]+Ry*Gn[1]+Rz*Gn[2])
+    ground.check(C, Rx, Ry, Rz)
+    ball.check(C, Rx, Ry, Rz)
 
-    # If ray intersects ground
-    intersects_ground=(lambda_ground > 0) & np.isfinite(lambda_ground)
+    intersects_ground_only=ground.intersects & np.logical_not(ball.intersects)
+    intersects_ball_only=ball.intersects & np.logical_not(ground.intersects)
+    intersects_ball_and_ground=ball.intersects & ground.intersects
+    intersects_ball_first=intersects_ball_only | (intersects_ball_and_ground & (ball.lmbda <= ground.lmbda))
+    intersects_ground_first=intersects_ground_only | (intersects_ball_and_ground & (ball.lmbda > ground.lmbda))
 
-    BC=C-B
+    ground_image=ground.image(C, Rx, Ry, Rz, L, intersects_ground_first)
+    ball_image=ball.image(C, Rx, Ry, Rz, L, intersects_ball_first)
 
-    RdotBC=Rx*BC[0]+Ry*BC[1]+Rz*BC[2]
 
-    # P = point of closest approach of ray to B
-    BPx=BC[0]-RdotBC*Rx
-    BPy=BC[1]-RdotBC*Ry
-    BPz=BC[2]-RdotBC*Rz
-    magBP=BPx**2+BPy**2+BPz**2
-
-    # If ray intersecs ball
-    intersects_ball=magBP <= ball_radius**2
-
-    # Lowest of two lambda values for location where rays intersect with surface of ball
-    beta=np.sqrt(RdotBC**2-np.linalg.norm(BC)**2+ball_radius**2) # Second term of quadratic eqn soln
-    if not np.array_equal(intersects_ball, np.isfinite(beta)):
-        raise RuntimeError("Expected intersections tests to be identical") # Sanity check
-    lambda_ball=-RdotBC-beta
-
-    intersects_ground_only=intersects_ground & np.logical_not(intersects_ball)
-    intersects_ball_only=intersects_ball & np.logical_not(intersects_ground)
-    intersects_ball_and_ground=intersects_ball & intersects_ground
-    intersects_ball_first=intersects_ball_only | (intersects_ball_and_ground & (lambda_ball <= lambda_ground))
-    intersects_ground_first=intersects_ground_only | (intersects_ball_and_ground & (lambda_ball > lambda_ground))
-
-    # S = first (closest to camera) location where ray intersects surface of ball
-    Sx=C[0]+lambda_ball*Rx
-    Sy=C[1]+lambda_ball*Ry
-    Sz=C[2]+lambda_ball*Rz
-
-    # SL
-    SLx=Sx-L[0]
-    SLy=Sy-L[1]
-    SLz=Sz-L[2]
-
-    # SB
-    SBx=Sx-B[0]
-    SBy=Sy-B[1]
-    SBz=Sz-B[2]
-    phi=np.arctan2(SBy, SBx)
-    theta=np.arctan2(SBz, np.sqrt(SBx**2+SBy**2))
-
-    # Normal to surface at S
-    magSB=np.sqrt(SBx**2+SBy**2+SBz**2)
-    magSL2=SLx**2+SLy**2+SLz**2
-    magSL=np.sqrt(magSL2)
-
-    # alpha = angle between normal to surface (outwards) and line from surface to L
-    cosalpha=-(SBx*SLx+SBy*SLy+SBz*SLz)/(magSB*magSL)
-
-    # Illumination of surface by light 1/R2 law and projection effect of angle
-    lfac=cosalpha/magSL2
-    lfac/=np.nanmax(lfac) # Normalise so max is 1
-
-    # If normal not pointing towards light then no illumination
-    lfac[lfac<0]=0
-
-    # Pattern on ball
-    dphi=np.pi/10
-    dtheta=np.pi/10
-    iphi=np.floor(phi/dphi).astype(int)
-    itheta=np.floor(theta/dtheta).astype(int)
-    w_noball=np.where(np.logical_not(intersects_ball_first))
-    red_ball=intersects_ball_first*np.mod(iphi+itheta, 2)*lfac
-    red_ball[w_noball]=0
-    white_ball=intersects_ball_first*np.mod(iphi+itheta+1, 2)*lfac
-    white_ball[w_noball]=0
-
-    # Point of intersection with ground
-    Gx=C[0]+lambda_ground*Rx
-    Gy=C[1]+lambda_ground*Ry
-    Gz=C[2]+lambda_ground*Rz
-
-    dx=3
-    dy=10
-    ix=np.floor(Gx/dx).astype(int)
-    iy=np.floor(Gy/dy).astype(int)
-    ground=intersects_ground_first*np.mod(ix+iy, 2)
-
-    ground_image=ImageToRGB(ground, rgb=[0, 0.3, 0], rgb0=[0, 0, 0])
-    red_ballimage=ImageToRGB(red_ball, rgb=[1, 0, 0], rgb0=[0, 0, 0])
-    white_ballimage=ImageToRGB(white_ball, rgb=[1, 1, 1], rgb0=[0, 0, 0])
-
-    ax.imshow(red_ballimage+white_ballimage+ground_image,
+    ax.imshow(ball_image+ground_image,
               interpolation='nearest',
               extent=[thetaHr[0], thetaHr[1], thetaVr[0], thetaVr[1]],
               origin='lower')
